3687959828028dae9af05a8e0f08bfe845be5a6c
MihirJain <mihirjain415@gmail.com>
2020-09-11 02:58:11 +0530

    New viw of html dynamically

--- toDoApp/polls/views.py
+++ toDoApp/polls/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, World. You are at polls index.")
+    myQuestion = Question
+    latestQuestionList = myQuestion.objects.order_by('-pubDate')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latestQuestionList': latestQuestionList
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s. " % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
