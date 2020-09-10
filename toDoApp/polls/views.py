28538307d88d58f3ef16aa414005f307222deed3
MihirJain <mihirjain415@gmail.com>
2020-09-11 01:21:59 +0530

    Updated

--- toDoApp/polls/views.py
+++ toDoApp/polls/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, World. You are at polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s. " % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of questions %s. "
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
